81cc6f1b63db7e1ad65a8393a6a13026f4b9469a
adwydman <adwydman@gmail.com>
2017-08-10 21:48:45 -0400

    add creating ssh key

--- after-install.sh
+++ after-install.sh
@@ -5,3 +5,8 @@ sudo apt update; sudo apt install atom
 sudo apt install curl
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
+ssh-keygen -t rsa -b 4096 -C "adwydman@gmail.com"
+eval "SSH_AUTH_SOCK=/tmp/ssh-qZyUdP7cSJUg/agent.13670; export SSH_AUTH_SOCK;
+SSH_AGENT_PID=13671; export SSH_AGENT_PID;
+echo Agent pid 13671;"
+ssh-add ~/.ssh/id_rsa
