81fc1743b01dcbfbc70f8229b9809552b465bffb
adwydman <adwydman@gmail.com>
2017-08-10 21:42:39 -0400

    initial

---
+++ after-install.sh
@@ -0,0 +1,7 @@
+sudo apt install firmware-b43-installer
+sudo apt-get install git-all
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt update; sudo apt install atom
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
